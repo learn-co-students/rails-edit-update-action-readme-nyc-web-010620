8f7c9add27bbcb96a2608eea855c5e50e3a222cb
github username <github email address>
2020-02-03 19:54:53 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,27 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
+    @article = Article.create!(article_params2)
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  # You always have to pass in an instance in edit controller action
+  def edit 
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    article = Article.find_by_id(params[:id])
+    article.update(article_params)
+    redirect_to article_path(article)
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  def article_params2
+    params.permit(:title, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Article.create!(title: "Twitter Goes Public: 21 Things You Should Know", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet tellus nisi, sit amet tristique odio scelerisque nec. Proin ante lectus, faucibus non elementum at, interdum quis massa. Sed ac tellus id nisi aliquet placerat.")
+a2 = Article.create!(title: "Fat Makes You Thin", description: "Aenean aliquet tellus nisi, sit amet tristique odio scelerisque nec. Faucibus non elementum at, interdum quis massa. Sed ac tellus id nisi aliquet placerat.")
+a3 = Article.create!(title: "Six Types Of Investors – Which Group Are You In?", description: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Aliquam in tincidunt justo.")
+a4 = Article.create!(title: "21 Ways to Dominate Youtube: The Ultimate Guide", description: "Phasellus tortor urna, cursus nec eros a, fermentum viverra nisl. Pellentesque tincidunt venenatis eros, nec dapibus tortor auctor vitae. In quis urna nulla.")
+a5 = Article.create!(title: "10 Step To Successfully Outsourcing Your Online Business", description: "Cras porttitor quis erat vel ullamcorper. Aliquam ante ex, lacinia fringilla malesuada ut, consequat eget nisi. Nulla rutrum accumsan libero, pellentesque porttitor eros.")
+a6 = Article.create!(title: "How I Doubled My Money With Facebook Ads", description: "Phasellus non mauris faucibus, aliquam elit sit amet, cursus purus. Pellentesque eu suscipit ligula, sollicitudin viverra purus. Aliquam rhoncus vitae nisl id tristique. Praesent in magna non odio feugiat volutpat.")
